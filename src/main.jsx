eaf48a7059b9845b552a2f2e7675f49459dc70db
Ryan Moore <moorek11c@gmail.com>
2024-10-02 22:42:32 -0700

    ghpages not working

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,8 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
